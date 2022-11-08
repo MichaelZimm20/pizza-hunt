89eb692693900c73bf3970e1215760c95d6eaecc
DJMIKE20 <m.zimm20@gmail.com>
2022-11-08 10:40:34 -0500

    format date on frontend and connected comment length to frontomment length to frontend and pizza

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,5 +1,8 @@
 const { Schema, model } = require('mongoose');
 
+// import dateFormat function
+const dateFormat = require('../utils/dateFormat');
+
 const PizzaSchema = new Schema (
     {
         pizzaName: {
@@ -10,7 +13,9 @@ const PizzaSchema = new Schema (
         },
         createdAt: {
             type: Date,
-            default: Date.now
+            default: Date.now,
+            get: (createdAtVal) => dateFormat(createdAtVal)
+
         },
         size: {
             type: String,
@@ -27,6 +32,7 @@ const PizzaSchema = new Schema (
     {
         toJSON: {
           virtuals: true,
+          getters: true
         },
         id: false
     }
